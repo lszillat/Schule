7d442b447a1a411095307bf04dfed1e77128d25d
lszillat <leonard.szillat@gmail.com>
2022-05-18 08:02:03 +0200

    Übung 1 18/05/2022

---
+++ src/Echo.java
@@ -0,0 +1,7 @@
+public class Echo {
+    public void antworten(String S, int l) {
+        for (int i = 0; i < l; i++) {
+            System.out.println(S);
+        }
+    }
+}
---
+++ src/Tester.java
@@ -0,0 +1,7 @@
+public class Tester {
+    public void rufen(String S, int l) {
+        for (int i = 0; i < l; i++) {
+            System.out.println(S);
+        }
+    }
+}
